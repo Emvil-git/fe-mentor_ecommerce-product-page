ca324c674e56033dbe9aeb206cd1e6964d9ef48e
Miguel Villanueva <emvil542@gmail.com>
2022-11-23 23:13:49 +0800

    feat(product): add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import './App.scss';
 import NavbarDesktop from './components/navbarDesktop/NavbarDesktop';
+import { useBEM } from './customHooks';
 
 function App() {
+
+  const [B,E] = useBEM('App')
+
   return (
-    <div className="App">
+    <div className={B()}>
       <NavbarDesktop/>
+      
+      <main>
+        <div className={E('prod')}>
+          <h2 className={E('prod-company')}>Sneaker Company</h2>
+
+          <h1 className={E('prod-name')}>Fall Limited Edition Sneakers</h1>
+
+          <p className={E('prod-desc')}>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+
+          <section className={E('prod-pricing')}>
+            <span className={E('prod-discount')}>
+              <h2 className={E('prod-discount-price')}>$125.00</h2>
+
+              <span className={E('prod-discount-rate')}>50%</span>
+            </span>
+
+            <p className={E('prod-baseprice')}>$250.00</p>
+          </section>
+        </div>
+      </main>
 
     </div>
   );
